c05ba6fbfb0efc773eac3dd64895cb72bfdee649
Jesús Bueno <38425590+jesus121@users.noreply.github.com>
2025-02-18 20:17:43 +0100

    Delete BuenoPeralesJesus_pruebatec4 directory

--- BuenoPeralesJesus_pruebatec4/src/main/java/com/example/BuenoPeralesJesus_pruebatec4/repository/HotelRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.BuenoPeralesJesus_pruebatec4.repository;
-
-import com.example.BuenoPeralesJesus_pruebatec4.model.Hotel;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface HotelRepository extends JpaRepository<Hotel, Long> {
-}
--- BuenoPeralesJesus_pruebatec4/src/main/java/com/example/BuenoPeralesJesus_pruebatec4/repository/ReservaVueloRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.BuenoPeralesJesus_pruebatec4.repository;
-
-import com.example.BuenoPeralesJesus_pruebatec4.model.ReservaVuelo;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ReservaVueloRepository  extends JpaRepository <ReservaVuelo , Long> {
-}
